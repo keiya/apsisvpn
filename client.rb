0268b97d56c68a88a8297e5379763fa5c86cb00b
keiya <keiya_21@yahoo.co.jp>
2016-12-05 18:26:33 +0000

    fixed

--- client.rb
+++ client.rb
@@ -13,7 +13,7 @@ class ApsisNode
   end
 
   def get_random_nodes()
-    @path = "nodes/random"
+    @path = "/nodes/random"
     r = RestClient.get(url=@url+@path,
                        :params => {:pubkey => @pubkey},
                        :headers => @headers.merge({}))
@@ -21,7 +21,7 @@ class ApsisNode
   end
 
   def post_node(payload)
-    @path = 'nodes'
+    @path = '/nodes'
     r = RestClient.post(url=@url+@path,
                     payload.to_json,
                     headers=@headers.merge({}))
@@ -40,13 +40,14 @@ class ApsisNode
 end
 
 class Tinc
-  def initialize(hosts_dir)
+  def initialize(hosts_dir, node)
     @hosts_dir = hosts_dir
+    @node = node
   end
 
-  def get_pubkey(node)
+  def pubkey()
     ed25519_token = 'Ed25519PublicKey = '
-    File.open("#{@hosts_dir}/#{node}") do |f|
+    File.open("#{@hosts_dir}/#{@node}") do |f|
       f.each_line do |l|
         if l.start_with?(ed25519_token)
           return l.slice(ed25519_token.length..l.length).chomp
@@ -54,19 +55,39 @@ class Tinc
       end
     end
   end
+
+  def host()
+    node_file = ''
+    File.open(@hosts_dir + '/' + @node) do |file|
+      node_file = file.read
+    end
+    node_file
+  end
+
+  def write_host(file)
+    #File.open(@hosts_dir + '/' + @node, "w") do |f|
+    File.open('./' + @node, "w") do |f|
+      f.puts(file)
+    end
+  end
 end
 
 config = YAML.load_file('config.yml')
 
-t = Tinc.new(config['tinc']['dir'])
-pubk = t.get_pubkey(config['tinc']['node']) # retrieve my own pubkey
+t = Tinc.new(config['tinc']['dir'],config['tinc']['node'])
 
-url = "http://apsis-exchange-hub/"
+pubk = t.pubkey
 
-an = ApsisNode.new(url, pubk)
+an = ApsisNode.new(config['hub']['url'], pubk)
 
-node = {keys: {ed25519: pubk}}
+node = {keys: {ed25519: pubk}, file: t.host}
 
 an.post_node(node) # publish own data to the server
 candidate_nodes = an.get_random_nodes # fetch a random node
-pp candidate_nodes
+candidate_nodes
+
+response = JSON.parse(candidate_nodes[:body])
+
+# this tinc instance is ours
+t = Tinc.new(config['tinc']['dir'], 'me')
+t.write_host(response['file'])
