24a25f6b9078d877a24a680244c11341dcab04aa
keiya <keiya_21@yahoo.co.jp>
2017-01-11 18:16:57 +0000

    proof of work

--- client.rb
+++ client.rb
@@ -4,8 +4,45 @@ require 'pp'
 require 'yaml'
 require 'optparse'
 require 'jwt'
+require 'digest/sha2'
 
-# TODO: separate
+class ProofOfWork
+  #@charset = (" ".."~").to_a
+  @charset = ("0".."9").to_a
+
+  def self.solve(target,maxlength)
+    for i in 1..maxlength+1
+      ProofOfWork.chain(ProofOfWork.product(i)) do |candidate|
+        hashval = Digest::SHA256.hexdigest(candidate)
+        if hashval.start_with?(target)
+          return candidate
+        end
+      end
+    end
+    return nil
+  end
+
+  def self.chain(*iterables)
+     for it in iterables
+         if it.instance_of? String
+             it.split("").each do |i|
+                 yield i
+             end
+         else
+             for elem in it
+                 yield elem
+             end
+         end
+     end
+  end
+
+  def self.product(max)
+    strings = 1.upto(max).flat_map do |n|
+      @charset.repeated_permutation(n).map(&:join)
+    end
+  end
+end
+
 class ApsisNode
   def initialize(url, pubkey)
     @url = url
@@ -19,7 +56,10 @@ class ApsisNode
                        :params => {:pubkey => @pubkey},
                        :headers => @headers.merge({}))
     resp = gen_response(r)
-    #JWT.decode resp[:body], nil, false
+    @jwt = JWT.decode resp[:body], nil, false
+    p @jwt[0]['data']['challenge']
+    @headers['X-PoW'] =  ProofOfWork.solve(@jwt[0]['data']['challenge'],5)
+    p @headers['X-PoW']
     @headers[:Authorization] = 'Bearer ' + resp[:body]
     resp
   end
